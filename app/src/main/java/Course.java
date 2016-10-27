e577938836d8ab97529783e7ab6870ff16289bed
Brendan McLaughlin <brendanmclaughlin20@gmail.com>
2016-10-27 16:40:17 -0400

    Added CourseFinder and Course classes to the project

---
+++ app/src/main/java/Course.java
@@ -0,0 +1,6 @@
+/**
+ * Created by BrendanMcLaughlin on 10/27/16.
+ */
+
+public class Course {
+}
---
+++ app/src/main/java/CourseFinder.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+/**
+ * Created by BrendanMcLaughlin on 10/27/16.
+ */
+
+public class CourseFinder {
+    ArrayList<Course> courses = new ArrayList<>();
+}
